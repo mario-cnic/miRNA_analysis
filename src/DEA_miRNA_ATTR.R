c038d7d78a2253b042477d980e0c678fb02ce92e
mario_cnic <mario.ruiz@cnic.es>
2024-07-02 11:36:47 +0200

    Added markdown files

--- src/DEA_miRNA_ATTR.R
+++ src/DEA_miRNA_ATTR.R
@@ -1,6 +1,3 @@
-
-
-
 ############## Installation ################
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
@@ -16,7 +13,7 @@ library(tidyverse,quietly = TRUE)
 
 counts_xlsx <- "~/Proyectos/miRNA_analysis/data/Novogene/X204SC24050930-Z01-F002_release/X204SC24050930-Z01-F002/02.Result_X204SC24050930-Z01-F002_Homo_sapiens/Result_X204SC24050930-Z01-F002_Homo_sapiens/Result_X204SC24050930-Z01-F002_Homo_sapiens/12.DiffExprAnalysis/12.1.miRNAExp/Readcount_TPM.xls"
 counts_csv_path <- "data/readcounts.csv"
-pre_counts <- read_xls(counts_xlsx)
+# pre_counts <- read_xls(counts_xlsx)
 # No puedo leer archivo, por que
 # file.exists(counts_xlsx)
 # format_from_ext(counts_xlsx)
@@ -25,7 +22,7 @@ pre_counts <- read_xls(counts_xlsx)
 ## Create readcount file from Novogene
 pre_counts2 <- read.delim(counts_xlsx)
 # Variable transformation
-pre_counts2 <- pre_counts2 %>% 
+pre_counts2 <- pre_counts2 %>%
   select(ends_with(".readcount")) %>%
   rename_with(~gsub(".readcount","",.x,fixed = TRUE))
 
@@ -40,7 +37,6 @@ metadata <- data.frame(condition)
 metadata <- rownames_to_column(metadata,"ID")
 
 write.csv(metadata, meta_csv_path,row.names = FALSE)
-
 
 ################ Diff Exp #####################
 readcounts <- read_csv(counts_csv_path,) %>% column_to_rownames("sRNA")
